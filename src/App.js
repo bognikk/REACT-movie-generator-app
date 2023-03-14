5e634ced29ebb043a2569d64771f7975eb6cdf4a
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-14 19:33:43 -0400

    POST request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.scss";
 
 function App() {
@@ -11,25 +12,28 @@ function App() {
 	const fetchMoviesHandler = useCallback(async () => {
 		setIsLoading(true);
 		setError(null);
-
 		try {
-			const response = await fetch("https://swapi.py4e.com/api/films/");
-
+			const response = await fetch(
+				"https://movie-generator-19624-default-rtdb.firebaseio.com/movies.json"
+			);
 			if (!response.ok) {
 				throw new Error("Something went wrong!");
 			}
 
 			const data = await response.json();
 
-			const transformedMoviesData = data.results.map((movieData) => {
-				return {
-					id: movieData.episode_id,
-					title: movieData.title,
-					openingText: movieData.opening_crawl,
-					releaseDate: movieData.release_date,
-				};
-			});
-			setMovies(transformedMoviesData);
+			const loadedMovies = [];
+
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					openingText: data[key].openingText,
+					releaseDate: data[key].releaseDate,
+				});
+			}
+
+			setMovies(loadedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
@@ -38,7 +42,22 @@ function App() {
 
 	useEffect(() => {
 		fetchMoviesHandler();
-	}, []);
+	}, [fetchMoviesHandler]);
+
+	async function addMovieHandler(movie) {
+		const response = await fetch(
+			"https://movie-generator-19624-default-rtdb.firebaseio.com/movies.json",
+			{
+				method: "POST",
+				body: JSON.stringify(movie),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const data = await response.json();
+		console.log(data);
+	}
 
 	let content = <p>Found no movies.</p>;
 
@@ -56,6 +75,9 @@ function App() {
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
